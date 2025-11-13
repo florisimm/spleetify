c05c48062dbb4e5e6eefdd6ef40b74daecf1e099
Floris Simmelink <florissimmelink@gmail.com>
2025-11-13 17:10:46 +0100

    Auto update analyse

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/spleetify/",     // nodig voor GitHub Pages (subfolder)
+  base: "/spleetify/",
   build: {
-    outDir: "docs",        // <<< HIER: build direct naar /docs
-  },
+    outDir: "docs"
+  }
 });
